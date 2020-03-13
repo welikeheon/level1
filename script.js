d0479dffdb03a469510b6a853fac2176cca29897
walt4771 <42925880+walt4771@users.noreply.github.com>
2020-03-13 22:19:25 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 function submit () {
-	fetch('', {
-		method: '',
+	var x = document.getElementById("n2").checked
+	var data = {answers: x};
+	
+	fetch('/submit', {
+		method: 'POST',
 		headers: {'Content-Type': 'application/json'},
-		body: JSON.stringify({ })
+		body: JSON.stringify({ data })
 	})
 		.then(res => res.json())
 		.then(ret => {
-			// Update Element
+			document.getElementById("result") == "정답입니다"
 		});
 };
